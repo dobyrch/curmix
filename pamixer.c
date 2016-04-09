e561a570567640bbb8719df5f8f3086ba0963c9b
Douglas Christman <DouglasChristman@gmail.com>
2016-04-09 15:44:00 -0400

    Don't allow curses to set foreground/background colors

--- pamixer.c
+++ pamixer.c
@@ -176,6 +176,7 @@ int main(void)
 
 	setlocale(LC_ALL, "");
 	initscr();
+	use_default_colors();
 	start_color();
 	init_pair(1, COLOR_GREEN, COLOR_GREEN);
 	init_pair(2, COLOR_GREEN, COLOR_YELLOW);
